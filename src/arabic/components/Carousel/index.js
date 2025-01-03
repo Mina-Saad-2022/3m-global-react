93ecdc1deefa6fd5c863dfb0c148080fc76c91ef
Mina SaȜd <73399220+Mina-Saad-2022@users.noreply.github.com>
2025-01-03 20:36:50 +0200

    edit carousel in arabic

--- src/arabic/components/Carousel/index.js
+++ src/arabic/components/Carousel/index.js
@@ -1,3 +1,65 @@
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const CarouselComponent = () => {
+//   return (
+//     <div className="carousel-container" style={carouselContainerStyle}>
+//       <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+//         <div className="carousel-inner">
+//           <div className="carousel-item active">
+//             <img className="d-block w-100" src="https://www2.0zz0.com/2024/12/21/22/999248171.jpeg" alt="First slide" />
+//           </div>
+//           <div className="carousel-item">
+//             <img className="d-block w-100" src="https://www2.0zz0.com/2024/12/21/22/529073265.jpg" alt="Second slide" />
+//           </div>
+//           <div className="carousel-item">
+//             <img className="d-block w-100" src="https://www2.0zz0.com/2025/01/03/17/909730796.jpg" alt="Second slide" />
+//           </div>
+//           <div className="carousel-item">
+//             <img className="d-block w-100" src="https://www2.0zz0.com/2025/01/03/17/582133527.jpg" alt="Second slide" />
+//           </div>
+//           <div className="carousel-item">
+//             <img className="d-block w-100" src="https://www2.0zz0.com/2025/01/03/17/735195301.jpg" alt="Second slide" />
+//           </div>
+//         </div>
+//         <div className="layout">
+//           <section id="hero" className="hero section light-background">
+//             <div className="container">
+//               <div className="row gy-12">
+//                 <div className="col-lg-8 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="zoom-out">
+//                   <h1 className="title_arabic p-2 title_company">
+//                     <span className="bolder">3M-GLOBAL</span><br />
+//                   </h1>
+//                   <h3 className="title_arabic short_title">الخدمات الزراعية والبيئية</h3>
+//                   <h3 className="arabic p-2 title_arabic short_title_arabic">
+//                     نهج الزراعة العضوية للحفاظ علي البيئة الزراعية المصرية
+//                   </h3>
+//                   <div className="d-flex">
+//                     <a href="https://www.youtube.com/watch?v=NMhXRfTmAX8" className="glightbox btn-watch-video d-flex align-items-center">
+//                       <span>مشاهدة الفيديو المقدمة</span>
+//                       <i className="bi bi-play-circle"></i>
+//                     </a>
+//                     <a href="#about" className="btn-get-started">البدء</a>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </section>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const carouselContainerStyle = {
+//   backgroundColor: '#f5f9ff',
+//   padding: '0',
+//   position: 'relative',
+//   overflow: 'hidden',
+// };
+
+// export default CarouselComponent;
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -5,29 +67,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const CarouselComponent = () => {
   return (
     <div className="carousel-container" style={carouselContainerStyle}>
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
         <div className="carousel-inner">
           <div className="carousel-item active">
             <img className="d-block w-100" src="https://www2.0zz0.com/2024/12/21/22/999248171.jpeg" alt="First slide" />
@@ -39,24 +79,6 @@ const CarouselComponent = () => {
             <img className="d-block w-100" src="https://www2.0zz0.com/2025/01/03/17/909730796.jpg" alt="Third slide" />
           </div>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
       <div className="layout">
         <section id="hero" className="hero section light-background">
