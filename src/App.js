e419f8c807f3ebea004b54d3350748129bdaafda
Mina SaȜd <73399220+Mina-Saad-2022@users.noreply.github.com>
2025-01-03 20:25:12 +0200

    some update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import ArabicIndex from './arabic/index';
-import EnglishPage from './english/index'; // تأكد من أن الصفحة الإنجليزية موجودة
+import EnglishPage from './english/index';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App = () => {
-  const location = useLocation(); // استخدام useLocation للحصول على الـ path الحالي
+  const location = useLocation();
 
   useEffect(() => {
-    // تحقق من الـ URL الحالي
     const isEnglish = location.pathname.includes('/english');
     
-    // تغيير الـ dir الخاص بالـ html
     document.documentElement.setAttribute('dir', isEnglish ? 'ltr' : 'rtl');
     
-    // تحديد ملف الـ CSS بناءً على اللغة
     const link = document.getElementById('language-stylesheet');
     if (isEnglish) {
-      link.href = 'assets/css/styleEnglish.css'; // استخدم ملف الـ CSS الخاص بالإنجليزي
+      link.href = 'assets/css/styleEnglish.css';
     } else {
-      link.href = 'assets/css/styleArabic.css'; // استخدم ملف الـ CSS الخاص بالعربي
+      link.href = 'assets/css/styleArabic.css';
     }
-  }, [location]); // إضافة location كـ dependency ليتم تحديثه عند تغيير الـ URL
+  }, [location]);
 
   return (
     <div>
       <link
         id="language-stylesheet"
         rel="stylesheet"
-        href="assets/css/styleArabic.css" // افتراضيًا يستخدم الـ CSS الخاص بالعربي
+        href="assets/css/styleArabic.css"
       />
       <Routes>
         <Route path="/" element={<ArabicIndex />} />
